a586a28a120077f167191fad3e1eaf37218da8ce
masat7353 <masat7353@gmail.com>
2024-02-29 12:44:48 +0000

    eslintの設定、prettierの設定追加

--- next.config.mjs
+++ next.config.mjs
@@ -3,13 +3,13 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   webpack: (config, context) => {
-      config.watchOptions = {
-          ignored: /node_modules/,
-          poll: 1000,
-          aggregateTimeout: 300
-      }
-      return config
-  }
-};
+    config.watchOptions = {
+      ignored: /node_modules/,
+      poll: 1000,
+      aggregateTimeout: 300,
+    }
+    return config
+  },
+}
 
-export default nextConfig;
+export default nextConfig
